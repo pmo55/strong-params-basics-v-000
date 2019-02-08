f8b3fbfa00a0aca871ddcfb96a8743844cd354ca
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-08 15:50:40 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(post_params)
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 	
 	private
 	
-	def post_params
-	  params.require(:post).permit(:title,:description)
+	def post_params(*args)
+	  params.require(:post).permit(*args)
 	end
 end
